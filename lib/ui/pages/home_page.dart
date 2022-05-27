6cf2afeb602f03afc53d0d0746b55019a99b267e
Suyai Slootmans <suyaislootmans@gmail.com>
2022-05-26 22:14:32 -0300

    first commit

---
+++ lib/ui/pages/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      color: Colors.red,
+    );
+  }
+}
