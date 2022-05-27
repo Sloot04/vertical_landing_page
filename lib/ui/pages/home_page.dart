64921a161da108cc1002e5c2381b7089a8ffb38e
Suyai Slootmans <suyaislootmans@gmail.com>
2022-05-27 10:21:45 -0300

    HomePage

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
 
+import 'package:vertical_landing_page/ui/views/about_view.dart';
+import 'package:vertical_landing_page/ui/views/contact_view.dart';
+import 'package:vertical_landing_page/ui/views/home_view.dart';
+import 'package:vertical_landing_page/ui/views/location_view.dart';
+import 'package:vertical_landing_page/ui/views/pricing_view.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 100,
-      height: 100,
-      color: Colors.pink,
+    return Scaffold(
+      body: Stack(
+        children: [
+          const _HomeBody(),
+          Positioned(
+              right: 20,
+              top: 20,
+              child: Container(
+                width: 150,
+                height: 50,
+                color: Colors.black,
+              ))
+        ],
+      ),
+    );
+  }
+}
+
+class _HomeBody extends StatelessWidget {
+  const _HomeBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: const [
+        HomeView(),
+        AboutView(),
+        PricingView(),
+        ContactView(),
+        LocationView()
+      ],
     );
   }
 }
