e513d243e95be852c59d954b5acab059a2f43603
Suyai Slootmans <suyaislootmans@gmail.com>
2022-05-27 12:47:25 -0300

    Opciones del menú

--- lib/ui/shared/custom_app_menu.dart
+++ lib/ui/shared/custom_app_menu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:vertical_landing_page/ui/shared/custom_menu_item.dart';
 
 class CustomAppMenu extends StatefulWidget {
   const CustomAppMenu({Key? key}) : super(key: key);
@@ -38,27 +39,59 @@ class _CustomAppMenuState extends State<CustomAppMenu>
         child: Container(
           padding: const EdgeInsets.symmetric(horizontal: 10),
           width: 150,
-          height: 50,
+          height: isOpen ? 310 : 50,
           color: Colors.black,
-          child: Row(
+          child: Column(
             children: [
-              AnimatedContainer(
-                duration: const Duration(milliseconds: 200),
-                curve: Curves.easeInOut,
-                width: isOpen ? 50 : 0,
-              ),
-              Text(
-                'Menú',
-                style: GoogleFonts.roboto(color: Colors.white, fontSize: 18),
-              ),
-              const Spacer(),
-              AnimatedIcon(
-                  icon: AnimatedIcons.menu_close,
-                  progress: controller,
-                  color: Colors.white),
+              _MenuTitle(isOpen: isOpen, controller: controller),
+              if (isOpen) ...[
+                CustomMenuItem(text: 'Home', onPressed: () {}),
+                CustomMenuItem(text: 'About', onPressed: () {}),
+                CustomMenuItem(text: 'Pricing', onPressed: () {}),
+                CustomMenuItem(text: 'Contact', onPressed: () {}),
+                CustomMenuItem(text: 'Location', onPressed: () {}),
+                const SizedBox(height: 10)
+              ]
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _MenuTitle extends StatelessWidget {
+  const _MenuTitle({
+    Key? key,
+    required this.isOpen,
+    required this.controller,
+  }) : super(key: key);
+
+  final bool isOpen;
+  final AnimationController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      height: 50,
+      child: Row(
+        children: [
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 200),
+            curve: Curves.easeInOut,
+            width: isOpen ? 50 : 0,
+          ),
+          Text(
+            'Menú',
+            style: GoogleFonts.roboto(color: Colors.white, fontSize: 18),
+          ),
+          const Spacer(),
+          AnimatedIcon(
+              icon: AnimatedIcons.menu_close,
+              progress: controller,
+              color: Colors.white),
+        ],
       ),
     );
   }
