d2b154fe79a3ef34126f98d8d589e7cd43c2381a
Suyai Slootmans <suyaislootmans@gmail.com>
2022-05-27 10:57:21 -0300

    menu superior

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 import 'package:vertical_landing_page/ui/views/about_view.dart';
@@ -5,6 +6,8 @@ import 'package:vertical_landing_page/ui/views/contact_view.dart';
 import 'package:vertical_landing_page/ui/views/home_view.dart';
 import 'package:vertical_landing_page/ui/views/location_view.dart';
 import 'package:vertical_landing_page/ui/views/pricing_view.dart';
+
+import '../shared/custom_app_menu.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,16 +18,13 @@ class HomePage extends StatelessWidget {
       body: Container(
         decoration: buildBoxDecoration(),
         child: Stack(
-          children: [
-             const _HomeBody(),
+          children: const [
+            _HomeBody(),
             Positioned(
-                right: 20,
-                top: 20,
-                child: Container(
-                  width: 150,
-                  height: 50,
-                  color: Colors.black,
-                ))
+              right: 20,
+              top: 20,
+              child: CustomAppMenu(),
+            ),
           ],
         ),
       ),
@@ -49,6 +49,10 @@ class _HomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageView(
+      physics: const BouncingScrollPhysics(),
+      scrollDirection: Axis.vertical,
+      scrollBehavior: ScrollConfiguration.of(context).copyWith(
+          dragDevices: {PointerDeviceKind.touch, PointerDeviceKind.mouse}),
       children: const [
         HomeView(),
         AboutView(),
