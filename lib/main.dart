89eb11d9a92d067c5ffdab83016401706fce6716
Suyai Slootmans <suyaislootmans@gmail.com>
2022-05-26 22:29:28 -0300

    rutas

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
-
-import 'ui/pages/home_page.dart';
+import 'package:vertical_landing_page/router/router.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    Flurorouter.configureRoutes();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      home: HomePage(),
+      title: 'Landing Page',
+      initialRoute: '/home',
+      onGenerateRoute: Flurorouter.router.generator,
     );
   }
 }
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget {
     return Container(
       width: 100,
       height: 100,
-      color: Colors.red,
+      color: Colors.pink,
     );
   }
 }
